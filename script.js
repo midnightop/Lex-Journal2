70a0f1e4e93262666310548be43ed74fbf23d211
Midnight <51527818+midnightop@users.noreply.github.com>
2026-04-08 22:30:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// simple click animation
-document.querySelectorAll("button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.style.opacity = "0.7";
-    setTimeout(() => {
-      btn.style.opacity = "1";
-    }, 200);
-  });
-});
+function openModal(type) {
+  const modal = document.getElementById("modal");
+  const title = document.getElementById("modal-title");
+  const text = document.getElementById("modal-text");
+
+  if (type === "kesavananda") {
+    title.innerText = "Kesavananda Bharati Case (1973)";
+    text.innerText = "This case established the Basic Structure Doctrine, limiting Parliament’s power to amend the Constitution.";
+  }
+
+  if (type === "privacy") {
+    title.innerText = "Right to Privacy Case (2017)";
+    text.innerText = "Supreme Court declared privacy as a fundamental right under Article 21.";
+  }
+
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
